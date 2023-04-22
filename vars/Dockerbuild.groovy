1da05adbc1934522685b4f7ddc4b2c1f64de4a32
unknown <ashitoshrakshe9@gmail.com>
2023-04-22 17:35:41 +0530

    Docker image  build steps added

--- vars/Dockerbuild.groovy
+++ vars/Dockerbuild.groovy
@@ -1,7 +1,7 @@
 def call (String project, String imagetag , String hubuser){
-    sh"""
-    docker image build -t $(hubUsers)/$(project) .
-    docker image tag $(hubUsers)/$(project) $(hubUsers)/$(project):$(ImageTag)
-     docker image tag $(hubUsers)/$(project) $(hubUsers)/$(project):latest
-    """
+    sh """
+    docker image build -t ${hubUsers}/${project} .
+    docker image tag ${hubUsers}/${project} ${hubUsers}/${project}:${ImageTag}
+     docker image tag ${hubUsers}/${project} ${hubUsers}/${project}:latest
+     """
 }
